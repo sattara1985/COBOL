4a78fc90b1afc0c903506af3c3740036c62bb563
sattara1985 <44877707+sattara1985@users.noreply.github.com>
2023-06-25 00:31:36 -0500

    Add files via upload

---
+++ hola_mundo.cbl
@@ -0,0 +1,23 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. escribir_nombre_del_programa.
+       AUTHOR. Programacion_steven
+       INSTALLATION. ESPECIFICAR DONDE ESTA EL PROGRAMA.
+       DATE-WRITTEN.25/06/2023
+       DATE-COMPILED.
+       REMARKS. ESCRIBIR COMENTARIOS DEL PROGRAMA.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            DISPLAY "HOLA MUNDO"
+
+            STOP RUN.
+       END PROGRAM escribir_nombre_del_programa.
