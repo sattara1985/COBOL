36c8829c097994c4042feaa70df6d766b336d12b
sattara1985 <44877707+sattara1985@users.noreply.github.com>
2023-06-25 03:01:41 -0500

    Add files via upload

---
+++ GO-TO.cbl
@@ -0,0 +1,45 @@
+      ******************************************************************
+      * Author:STEVEN ATTARA
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. GOTO.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+      *VARIABLE DE UNA SOLA LETRA
+       01 SI-O-NO PIC X.
+
+       PROCEDURE DIVISION.
+
+
+           PREGUNTA.
+      *PERFORM__
+           PERFORM CONTINUACION.
+
+           IF SI-O-NO = "N" OR SI-O-NO ="n"
+               GO TO FINALIZA-PROGRAMA.
+           IF SI-O-NO ="S" OR SI-O-NO ="s"
+               PERFORM PROGRAMA
+           ELSE
+               DISPLAY "POR FAVOR, INTRODUZCA UNA 'N' O 'S'".
+
+      *RUTINAS
+           FINALIZA-PROGRAMA.
+               DISPLAY"ESCOGIO " SI-O-NO " NOS VEMOS PRONTO".
+               STOP RUN.
+
+           CONTINUACION.
+               DISPLAY "EJECUTAR PROGRAMA? (S/N)".
+               ACCEPT SI-O-NO.
+
+           PROGRAMA.
+               DISPLAY "SE EJECUTA EL PROGRAMA.".
+               GO TO CONTINUACION.
+
+
+
+
+       END PROGRAM GOTO.
