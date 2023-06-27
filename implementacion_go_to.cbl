bca870fa5822a7f95dca4f53a47e0910db4455d3
sattara1985 <44877707+sattara1985@users.noreply.github.com>
2023-06-26 23:27:39 -0500

    Add files via upload

---
+++ implementacion_go_to.cbl
@@ -0,0 +1,47 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. implementar_go_to.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+      *VARIABLE DE UNA SOLA LETRA
+       01 SI-O-NO PIC X.
+       PROCEDURE DIVISION.
+      *MAIN-PROCEDURE.
+      *DISPLAY "Hello world"
+           PREGUNTA.
+      *PERFORM__
+           PERFORM CONTINUACION.
+
+           IF SI-O-NO = "N" OR SI-O-NO ="n"
+               GO TO FINALIZA-PROGRAMA.
+           IF SI-O-NO ="S" OR SI-O-NO ="s"
+               PERFORM PROGRAMA
+           ELSE
+               DISPLAY "POR FAVOR, INTRODUZCA UNA 'N' O 'S'".
+
+
+
+      *RUTINAS
+           FINALIZA-PROGRAMA.
+               DISPLAY"ESCOGIO " SI-O-NO " NOS VEMOS PRONTO"
+               STOP RUN.
+
+           CONTINUACION.
+               DISPLAY "EJECUTAR PROGRAMA? (S / N )"
+               ACCEPT SI-O-NO
+               DISPLAY "INGRESO: "SI-O-NO
+               PERFORM CONTINUACION.
+
+           PROGRAMA.
+               DISPLAY "SE EJECUTA EL PROGRAMA.ghgfhg".
+               PERFORM CONTINUACION.
+
+
+
+       END PROGRAM implementar_go_to.
